cc68da29d4644700aebd8e692a60e2a42f42ec72
im5439 <57483743+im5439@users.noreply.github.com>
2019-12-23 10:24:57 +0900

    Add files via upload

--- AccountingApp.java
+++ AccountingApp.java
@@ -3,29 +3,40 @@ import java.util.Scanner;
 public class AccountingApp {
 	
 	public static void main(String[] args) {
-		
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("판매자의 공급가를 입력하세요.");
-		System.out.print("$ > ");
-		double ValueOfSupply = scanner.nextDouble();
-		
-		double VAT = (ValueOfSupply * 0.1);
-		double Expense = (ValueOfSupply * 0.3);
-		double Income = (ValueOfSupply * 0.7);
-		double dividend1 = (ValueOfSupply * 0.5);
-		double dividend2 = (ValueOfSupply * 0.3);
-		double dividend3 = (ValueOfSupply * 0.2);
-		
-		System.out.println("공급가 : "+ ValueOfSupply + "원");
-		System.out.println("판매가 : "+ (ValueOfSupply+VAT)+"원");
-		System.out.println("비용 : "+ Expense +"원");
-		System.out.println("이익 : "+ Income +"원");
-		System.out.println("dividend1 : "+ dividend1 +"원");
-		System.out.println("dividend2 : "+ dividend2 +"원");
-		System.out.println("dividend3 : "+ dividend3 +"원");
-		
-		scanner.close();
-		
+
+		while (true) {
+			Scanner scanner = new Scanner(System.in);
+			System.out.println("판매자의 공급가를 입력하세요.");
+			System.out.print("$ > ");
+			double ValueOfSupply = scanner.nextDouble();
+			double VAT = (ValueOfSupply * 0.1);
+			double Expense = (ValueOfSupply * 0.3);
+			double Income = (ValueOfSupply * 0.7);
+			double[] dividendRate = new double[3];
+			dividendRate[0] = 0.5;
+			dividendRate[1] = 0.3;
+			dividendRate[2] = 0.2;
+			
+			if (ValueOfSupply > 1) {
+				System.out.println("공급가 : " + ValueOfSupply + "원");
+				System.out.println("판매가 : " + (ValueOfSupply + VAT) + "원");
+				System.out.println("부가세 : " + VAT + "원");
+				System.out.println("비용 : " + Expense + "원");
+				System.out.println("이익 : " + Income + "원");
+				System.out.println("dividend1 : " + (Income * dividendRate[0]) + "원");
+				System.out.println("dividend2 : " + (Income * dividendRate[1]) + "원");
+				System.out.println("dividend3 : " + (Income * dividendRate[2]) + "원");
+				continue;
+				
+			} else if (ValueOfSupply < 0) {
+				break;
+			}
+			scanner.close();
+		}
+		System.out.println("<EXIT>");
+
 	}
+
+	
 
 }
